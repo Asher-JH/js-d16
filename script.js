9c18f00284cdf0275e02a5560ccbbf6f2fcb4157
Asher <asherchan2002@hotmail.com>
2020-08-07 15:39:47 +0800

    updates

---
+++ script.js
@@ -0,0 +1,19 @@
+let progress = 0;
+
+function loading() {
+	progress++ ;
+
+	if(progress / 100 == 1) {
+		window.clearInterval(interval)
+	}
+	$('#loadingBar').css('width', progress)
+	$('#progress').html(`${progress}%`)
+}
+
+
+let status = 0;
+$('#runBtn').on('click', () => {
+	interval = window.setInterval(loading, 1000)
+
+	
+})
